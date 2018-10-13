fcaa1147377171d4620cfb9e24096d90ead4bc63
gaku moriya <gaku.moriya@github.com>
2018-10-14 02:46:55 +0900

    デバッグカメラ作成

---
+++ Assets/Resources/Scripts/CameraMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    
+    [SerializeField, Range(0.01f, 20.0f)]
+    private float moveSpeed = 10f;
+    private Vector3 prePosition;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        // ホイールクリックした時
+        if (Input.GetMouseButtonDown(2))
+        {
+            prePosition = Input.mousePosition;
+        }
+        // ホイールドラッグした時
+        else if (Input.GetMouseButton(2))
+        {
+            // 差分計算
+            Vector3 axis = Input.mousePosition - prePosition;
+
+            // 補正
+            if (axis.magnitude < Vector3.kEpsilon)
+                return;
+
+            // 平行移動
+            transform.Translate(-axis * Time.deltaTime * moveSpeed);
+            
+            // 保存
+            prePosition = Input.mousePosition;
+        }
+    }
+}
