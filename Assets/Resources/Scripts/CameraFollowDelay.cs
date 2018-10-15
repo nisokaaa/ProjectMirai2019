245577853406242e71b712236b92b0d6db6aff35
gaku moriya <gaku.moriya@github.com>
2018-10-15 11:02:38 +0900

    InputManagerをXBoxコンに対応

---
+++ Assets/Resources/Scripts/CameraFollowDelay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowDelay : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField, Range(1f, 20f)]
+    public float _distance;
+
+    [SerializeField, Range(1f, 20f)]
+    public float offsetY;
+
+    private Vector3 _offset;
+        
+    private Vector3 _lookDown = new Vector3(10f, 0f, 0f);
+    private const float _followRate = 0.1f;
+
+    void Start()
+    {
+        _offset = new Vector3(0f, offsetY, -_distance);
+
+        transform.position = target.TransformPoint(_offset);
+        transform.LookAt(target, Vector3.up);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.TransformPoint(_offset);
+        Vector3 lerp = Vector3.Lerp(transform.position, desiredPosition, _followRate);
+        Vector3 toTarget = target.position - lerp;
+        toTarget.Normalize();
+        toTarget *= _distance;
+        transform.position = target.position - toTarget;
+        transform.LookAt(target, Vector3.up);
+        transform.Rotate(_lookDown);
+    }
+}
---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Player移動クラス
+/// GakuMoriya
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float moveX, moveZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("D_Pad_H") * speed;
+        moveZ = Input.GetAxis("D_Pad_V") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+
+        rb.AddForce(new Vector3(moveX, 0, moveZ), ForceMode.Force);
+    }
+}
