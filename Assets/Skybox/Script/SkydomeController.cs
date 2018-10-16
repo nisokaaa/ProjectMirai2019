441297ea8abb8de229a4001db9a4538386468d44
shimura Masaki <operationsim.game@gmail.com>
2018-10-16 20:04:38 +0900

    【Skybox、skydomeControllerのPrefab化】【Skydomeをメインカメラに追従するようにした】 ・Skydome、skyboxのPrefab化に伴い、「SkyManager」を親とし、子要素にControllerを置くようにした。

--- Assets/Skybox/Script/SkydomeController.cs
+++ Assets/Skybox/Script/SkydomeController.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class SkydomeController : MonoBehaviour {
 
-    readonly string skyLiColorName = "Offset";               //MaterialのColorを設定するときのパラメータ名
     [SerializeField] Material _skyboxMaterial;                  //編集マテリアル
     float cnt = 0.0f;
+
     void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        cnt += 0.01f;
-        if(cnt >= 1.0f)
-        {
-            cnt = 0.0f;
-        }
-        _skyboxMaterial.SetFloat(skyLiColorName, cnt);
+        //メインカメラの座標を取得して、そこにスカイドームの位置を上書きする
+        transform.position = Camera.main.transform.position;
     }
 }
