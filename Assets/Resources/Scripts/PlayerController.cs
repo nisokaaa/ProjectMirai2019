42fbccfc28099d9373f270b3a1659eb6fc81f8d0
gaku moriya <gaku.moriya@github.com>
2018-10-16 22:45:16 +0900

    キーボード対応 修正1

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -51,46 +51,4 @@ public class PlayerController : MonoBehaviour
         // Rayが地面にあたってないときは速度減衰をやわらげよう・・・・
         Debug.Log(rb.velocity);
     }
-
-    //// 速度
-    //[SerializeField, Range(1f, 150f)]
-    //float speed = 50f;
-    //float speedCurrent = 0.0f;
-
-    //// 減衰率
-    //[SerializeField, Range(1f, 200f)]
-    //float moveForceMultiplier = 20f;
-
-    //// アクセル状態
-    //bool bAccelerator = false;
-
-    //void Update()
-    //{
-    //    // アクセルボタンInput
-    //    bAccelerator = Input.GetKey("joystick button 5") ? true : false;
-
-    //    // 回転
-    //    transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("L_Stick_H"));
-
-    //    // forwardのRay
-    //    Ray ray = new Ray(transform.position, transform.forward);
-    //    Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
-    //}
-
-    //void FixedUpdate()
-    //{
-    //    Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-
-    //    // アクセル押下してたら速度代入
-    //    speedCurrent = bAccelerator ? speed : 0.0f;
-
-    //    Vector3 moveVector = Vector3.zero;
-    //    moveVector = speedCurrent * transform.forward;
-
-    //    // ボタン非押下で勝手に速度減衰
-    //    rb.AddForce(moveForceMultiplier * (moveVector - rb.velocity));
-
-    //    // Rayが地面にあたってないときは速度減衰をやわらげよう・・・・
-    //    Debug.Log(rb.velocity);
-    //}
 }
