115f3b883c9bc0f951e3e53839ef247f8d7e5057
shimura Masaki <operationsim.game@gmail.com>
2018-10-19 12:36:43 +0900

    【SceneChangeデータをインポート】

---
+++ Assets/SceneChange/Script/SceneChangeMgr.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 「SceneChange関係のデータを保持しているゲームオブジェクト用のクラス」
+/// 引数　 ：
+/// 戻り値 ：
+/// １．
+/// ２．
+/// 作成者：志村まさき
+/// </summary>
+public class SceneChangeMgr : SingletonMonoBehaviour<SceneChangeMgr>
+{
+    public void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //テストコード
+        /*
+        //次のシーンの読み込み処理
+		if(Input.GetKeyDown(KeyCode.Q))
+        {
+            SceneChangeController.Instance.SetChangeScene("Title");
+        }
+
+        //シーンの切り替え実行
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            SceneChangeController.Instance.SetChangeSceneExecution();
+        }
+        */
+	}
+    
+}
