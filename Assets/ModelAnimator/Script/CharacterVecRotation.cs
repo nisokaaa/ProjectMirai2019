4d280abccb54b3bf3a6ad71d77763754cd237ab4
shimura Masaki <operationsim.game@gmail.com>
2018-10-21 17:13:55 +0900

    fast commit ・「ModelAnimator」インポート

---
+++ Assets/ModelAnimator/Script/CharacterVecRotation.cs
@@ -0,0 +1,60 @@
+﻿//キャラクターのベクトルを取得してオブジェクトを回転させる
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 「キャラクターの移動ベクトルを見て、その方向に回転させる」
+/// 引数　 ：解説
+/// 戻り値 ：解説
+/// １．手順
+/// ２．手順
+/// ３．手順
+/// 作成者：志村まさき
+/// </summary>
+public class CharacterVecRotation : MonoBehaviour {
+
+    [SerializeField] GameObject player;
+    [SerializeField] Vector3 playerVecCharacterPos;
+
+	// Use this for initialization
+	void Start () {
+        //ベクトルの起点
+        playerVecCharacterPos = player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void VecMoveRotation()
+    {
+        //移動方向ベクトルの取得
+        Vector3 charVec = playerVecCharacterPos - transform.position;
+
+        if (charVec.magnitude > 0.01f)
+        {
+            //回転量を関数によって取得し、rotationに入れる
+            transform.rotation = Quaternion.LookRotation(charVec);
+        }
+
+        //座標の更新
+        playerVecCharacterPos = transform.position;
+    }
+
+    //第一引数のポジションの方向に自分が向く
+    public void SetPosRotation( Vector3 Position)
+    {
+        ////移動方向ベクトルの取得
+        //Vector3 charVec = transform.position - Position;
+
+        //if (charVec.magnitude > 0.01f)
+        //{
+        //    //回転量を関数によって取得し、rotationに入れる
+        //    transform.rotation = Quaternion.LookRotation(charVec);
+        //}
+        ////座標の更新
+        //playerVecCharacterPos = transform.position;
+    }
+}
---
+++ Assets/ModelAnimator/Script/PlayerColliderCheck.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 「フィールドに立っているか確認するクラス」
+/// 引数　 ：解説
+/// 戻り値 ：解説
+/// １．手順
+/// ２．手順
+/// ３．手順
+/// 作成者：志村まさき
+/// </summary>
+public class PlayerColliderCheck : MonoBehaviour {
+
+    [SerializeField] bool tagPlane_CollisionEnter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// オブジェクトが当たっているかちぇっくする
+    /// </summary>
+    /// <returns></returns>
+    public bool GetCollisionEnterExit()
+    {
+        return tagPlane_CollisionEnter;
+    }
+
+    //衝突したとき
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Plane")
+        {
+            tagPlane_CollisionEnter = true;
+        }
+    }
+
+    //離れた時
+    private void OnCollisionExit(Collision collision)
+    {
+        tagPlane_CollisionEnter = false;
+    }
+
+    //振れている間
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    
+    //}
+}
