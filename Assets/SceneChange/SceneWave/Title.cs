1fa29b5a22cfec142037cac8fb541d5b323c9a5b
shimura Masaki <operationsim.game@gmail.com>
2018-11-04 15:17:48 +0900

    【タイトルシーンからゲームシーンに遷移】 ・何かボタンが押されたらゲームシーンに行く

---
+++ Assets/SceneChange/SceneWave/Title.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 「タイトル処理」
+/// 流れ
+/// １．タイトル開始演出
+/// ２．通常タイトル表示
+/// ３．タイトル終了演出
+/// 作成者：名前
+/// </summary>
+public class Title : MonoBehaviour {
+
+    enum WAVE
+    {
+        NONE = 0,
+        START,
+        PLAY,
+        END,
+        MAX
+    };
+
+    [SerializeField] WAVE title = WAVE.NONE;
+
+    // Use this for initialization
+    void Start () {
+        title = WAVE.START;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(title)
+        {
+            case WAVE.START:
+                SceneChangeController.Instance.SetChangeScene("Game");
+                title = WAVE.PLAY;
+                break;
+            case WAVE.PLAY:
+                if(Input.anyKey)
+                {
+                    title = WAVE.END;
+                }
+                break;
+            case WAVE.END:
+                SceneChangeController.Instance.SetChangeSceneExecution();
+                break;
+        }
+    }
+}
