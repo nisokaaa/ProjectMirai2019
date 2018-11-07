a363c68974347e5df0e6bc95bc4dac1cfb0830a3
shimura Masaki <operationsim.game@gmail.com>
2018-11-07 21:26:31 +0900

    【ボス追従処理】

--- Assets/BossController.cs
+++ Assets/BossController.cs
@@ -14,20 +14,24 @@ public class BossController : MonoBehaviour {
         END,        //退却処理
     };
 
-    [SerializeField] STATE state;
-    [SerializeField] Vector3 SeekPos;
+    public Transform m_target = null;
+    public float m_speed = 5;
+    public float m_attenuation = 0.5f;
+    [SerializeField] Vector3 SeecPos;
+
+    private Vector3 m_velocity;
+
 
 
     // Use this for initialization
     void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(
-            PlayerGameObject.transform.position.x + SeekPos.x,
-            PlayerGameObject.transform.position.y + SeekPos.y,
-            PlayerGameObject.transform.position.z + SeekPos.z);
-	}
+        m_velocity += ((m_target.position - SeecPos) - transform.position) * m_speed;
+        m_velocity *= m_attenuation;
+        transform.position += m_velocity *= Time.deltaTime;
+    }
 }
