e29abebf367c11e4ffd051228d752b3bbc715fe9
shimura Masaki <operationsim.game@gmail.com>
2018-11-17 17:55:36 +0900

    【スローモーション処理実装】 ・全体のスローモーション

---
+++ Assets/SceneChange/TimeAnimation/HitStopDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitStopDamage : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject damagePrefab;
+
+    [SerializeField]
+    private TimeManager timeManager;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Obstacle")
+        {
+            var damageParticle = GameObject.Instantiate(damagePrefab, col.ClosestPointOnBounds(col.transform.position), Quaternion.identity) as GameObject;
+            //　全体のタイムスケールを変更する
+            timeManager.SlowDown();
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Obstacle")
+        {
+            var damageParticle = GameObject.Instantiate(damagePrefab, transform.position, Quaternion.identity) as GameObject;
+            //　全体のタイムスケールを変更する
+            timeManager.SlowDown();
+        }
+    }
+}
---
+++ Assets/SceneChange/TimeAnimation/HitStopDestroyGameObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class HitStopDestroyGameObject : MonoBehaviour
+{
+    private ParticleSystem particle;
+    // Use this for initialization
+    void Start()
+    {
+        particle = GetComponentInChildren<ParticleSystem>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (particle.isStopped)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
