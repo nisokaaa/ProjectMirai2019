7aec03caad0281bf19fdadfd771344869fcec339
shimura Masaki <operationsim.game@gmail.com>
2018-11-19 16:09:27 +0900

    【ビーム修正途中】

---
+++ Assets/Resources/Game/Script/SetPositionSeek.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPositionSeek : MonoBehaviour {
+
+    [SerializeField] Transform SetPosition;
+    [SerializeField] Vector3 SeekPosition;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(SetPosition.position.x + SeekPosition.x, SetPosition.position.y + SeekPosition.y, SetPosition.position.z + SeekPosition.z);
+	}
+}
